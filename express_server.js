4e45781f1566288547a026cad7bd1c6ecb2598bc
keefu <cyuplia@gmail.com>
2019-06-04 21:15:34 +0000

    Display URLs in EJS template

--- express_server.js
+++ express_server.js
@@ -15,8 +15,19 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+//app.get("/hello", (req, res) => {
+//  res.send("<html><body>Hello <b>World</b></body></html>\n");
+//});
+//app.get("/urls", (req, res) => {
+//  res.render("../views/urls_index.ejs")
+//})
+//app.get("/urls", (req, res) => {
+//  let templateVars = { urls: urlDatabase };
+//  res.render("urls_index", templateVars);
+//});
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

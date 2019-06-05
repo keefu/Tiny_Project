5566f6df808984f1ee18ed2f458c9cd60978a2b3
keefu <cyuplia@gmail.com>
2019-06-05 01:24:19 +0000

    redirect after form submission

--- express_server.js
+++ express_server.js
@@ -2,6 +2,19 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+function generateRandomString(length) {
+   var result           = '';
+   var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -12,6 +25,9 @@ var urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -19,19 +35,19 @@ app.get("/urls.json", (req, res) => {
 //  res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
 //app.get("/urls", (req, res) => {
-//  res.render("../views/urls_index.ejs")
+//  res.render("../views/urls_news.ejs")
 //})
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
-
   const keys = Object.keys(urlDatabase);
-
   const templateVars = {
     urls: urlDatabase,
     keys: keys
   };
   console.log(templateVars);
-
   res.render("urls_index", templateVars);
 });
 
@@ -45,6 +61,19 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  let short = generateRandomString(6);
+  let long = req.body.longURL;
+  console.log(short);
+  console.log(long);
+  urlDatabase[short] = long;
+  console.log(urlDatabase);
+  res.redirect(`urls/${short}`)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
